bbca887cad18d1e75cbf5df22f9ae1b2b9a97c56
Ryan Dakin <ryandakin42@gmail.com>
2021-09-29 12:44:58 -0400

    queries

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const mysql = require('mysql2');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,10 +9,37 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'SQL-2020-3030',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+
+// GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(row);
+  });
+
+// Delete a candidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
   });
 
 // Default response for any other request (Not Found)
